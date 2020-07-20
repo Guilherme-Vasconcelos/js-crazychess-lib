76c348b64762faf1fe2759c86bac9bf67f55cf1b
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2020-07-20 04:30:48 -0300

    Added docstrings

--- src/board.js
+++ src/board.js
@@ -1,7 +1,14 @@
 import { Rook, Bishop, Knight, King, Queen, Pawn, NullPiece } from './pieces.js';
 
+/**
+ * Class used to represent the chess board.
+ */
 class Board {
-    constructor(initialPositionFEN) {
+    /**
+     * Creates a board (8x8 matrix) and fills all positions with a NullPiece.
+     * @TODO the board should take as input the initial position FEN.
+     */
+    constructor() {
         this._piecesBoard = [];
         for (let i = 0; i < 8; ++i) {
             let currentRow = []
@@ -13,15 +20,29 @@ class Board {
         }
     }
 
+    /**
+     * Shows the current board.
+     */
     showBoard() {
         let output = ''
         for (let i = 0; i < 8; ++i) {
             for (let j = 0; j < 8; ++j) {
-                output += this._piecesBoard[i][j].name;
+                output += ' ' + this._piecesBoard[i][j].name + ' ';
             }
             output += '\n';
         }
         console.log(output);
+    }
+
+    /**
+     * Places a piece at a given position
+     * @param piece Piece to be placed. This must belong to a valid Piece class,
+     * such as Rook, Bishop, etc. 
+     * @param {string} position Position to be placed (in algebraic notation),
+     * e.g.: 'e4', 'a1', etc.
+     */
+    place(piece, position) {
+
     }
 }
 
--- src/pieces.js
+++ src/pieces.js
@@ -1,4 +1,10 @@
+/**
+ * Base class for Piece classes. Should not be used directly by the user.
+ */
 class Piece {
+    /**
+     * Creates a chess piece with available legal squares to move, name, etc
+     */
     constructor() {
         this.legalSquares = [];
         this.name;
@@ -6,52 +12,165 @@ class Piece {
     }
 }
 
+/**
+ * Class used to represent the Rook piece. Inherits from Piece.
+ */
 class Rook extends Piece {
-    constructor() {
+    /**
+     * Creates a rook
+     * @param {string} color color of your chess piece.
+     * Your chess piece must be either 'white' or 'black'. 
+     */
+    constructor(color) {
         super();
-        this.name = 'Rook';
+        switch (color) {
+            case 'white':
+                this.name = 'R';
+                break;
+            case 'black':
+                this.name = 'r';
+                break;
+            default:
+                throw new Error('Invalid color ' + color + '. A chess piece' +
+                    'must either be \'white\' or \'black\'.');  
+        }
     }
 }
 
+/**
+ * Class used to represent the Bishop piece. Inherits from Piece.
+ */
 class Bishop extends Piece {
-    constructor() {
+    /**
+     * Creates a bishop
+     * @param {string} color color of your chess piece.
+     * Your chess piece must be either 'white' or 'black'. 
+     */
+    constructor(color) {
         super();
-        this.name = 'Bishop';
+        switch (color) {
+            case 'white':
+                this.name = 'B';
+                break;
+            case 'black':
+                this.name = 'b';
+                break;
+            default:
+                throw new Error('Invalid color ' + color + '. A chess piece' +
+                    'must either be \'white\' or \'black\'.');  
+        }
     }
 }
 
+/**
+ * Class used to represent the Knight piece. Inherits from Piece.
+ */
 class Knight extends Piece {
-    constructor() {
+    /**
+     * Creates a knight
+     * @param {string} color color of your chess piece.
+     * Your chess piece must be either 'white' or 'black'. 
+     */
+    constructor(color) {
         super();
-        this.name = 'Knight';
+        switch (color) {
+            case 'white':
+                this.name = 'N';
+                break;
+            case 'black':
+                this.name = 'n';
+                break;
+            default:
+                throw new Error('Invalid color ' + color + '. A chess piece' +
+                    'must either be \'white\' or \'black\'.');  
+        }
     }
 }
 
+/**
+ * Class used to represent the King piece. Inherits from Piece.
+ */
 class King extends Piece {
-    constructor() {
+    /**
+     * Creates a king
+     * @param {string} color color of your chess piece.
+     * Your chess piece must be either 'white' or 'black'. 
+     */
+    constructor(color) {
         super();
-        this.name = 'King';
+        switch (color) {
+            case 'white':
+                this.name = 'K';
+                break;
+            case 'black':
+                this.name = 'k';
+                break;
+            default:
+                throw new Error('Invalid color ' + color + '. A chess piece' +
+                    'must either be \'white\' or \'black\'.');  
+        }
     }
 }
 
+/**
+ * Class used to represent the Queen piece. Inherits from Piece.
+ */
 class Queen extends Piece {
-    constructor() {
+    /**
+     * Creates a queen
+     * @param {string} color color of your chess piece.
+     * Your chess piece must be either 'white' or 'black'. 
+     */
+    constructor(color) {
         super();
-        this.name = 'Queen';
+        switch (color) {
+            case 'white':
+                this.name = 'Q';
+                break;
+            case 'black':
+                this.name = 'q';
+                break;
+            default:
+                throw new Error('Invalid color ' + color + '. A chess piece' +
+                    'must either be \'white\' or \'black\'.');  
+        }
     }
 }
 
+/**
+ * Class used to represent the Pawn piece. Inherits from Piece.
+ */
 class Pawn extends Piece {
-    constructor() {
+    /**
+     * Creates a pawn
+     * @param {string} color color of your chess piece.
+     * Your chess piece must be either 'white' or 'black'. 
+     */
+    constructor(color) {
         super();
-        this.name = 'Pawn';
+        switch (color) {
+            case 'white':
+                this.name = 'P';
+                break;
+            case 'black':
+                this.name = 'p';
+                break;
+            default:
+                throw new Error('Invalid color ' + color + '. A chess piece' +
+                    'must either be \'white\' or \'black\'.');  
+        }
     }
 }
 
+/**
+ * Class used to represent the NullPiece piece. Inherits from Piece.
+ * A NullPiece does nothing. It is just a placeholder for empty positions
+ * at the board.
+ */
 class NullPiece extends Piece {
     constructor() {
         super();
-        this.name = ' . ';
+        this.name = '.';
     }
 }
 
