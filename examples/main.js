47e20772d5f697f1ced64f73e12c9350475064ef
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2020-07-20 04:49:31 -0300

    Added placePiece and getPiece methods

--- examples/main.js
+++ examples/main.js
@@ -1,4 +1,11 @@
 import Board from '../src/board.js';
+// REMOVE THIS LATER
+import { Rook } from '../src/pieces.js';
 
+let rook = new Rook('white');
 let board = new Board();
+
+board.placePiece(rook, 'e4');
 board.showBoard();
+console.log(board.getPiece('e5'));
+console.log(board._piecesBoard[4][4]);
--- src/board.js
+++ src/board.js
@@ -36,13 +36,26 @@ class Board {
 
     /**
      * Places a piece at a given position
-     * @param piece Piece to be placed. This must belong to a valid Piece class,
+     * @param piece Piece to be placed. This must belong to a valid Piece class
      * such as Rook, Bishop, etc. 
      * @param {string} position Position to be placed (in algebraic notation),
      * e.g.: 'e4', 'a1', etc.
      */
-    place(piece, position) {
+    placePiece(piece, position) {
+        // @TODO: make this work, problem is the - '0' part. Same goes for below.
+        this._piecesBoard[8 - (position[1] - '0')][parseInt(position[0], 10) - 97] = piece;
+    }
 
+    /**
+     * Returns a piece from a given position
+     * @param {string} position Position to be searched (in algebraic notation)
+     * e.g.: 'e4', 'a1', etc.
+     */
+    getPiece(position) {
+        // @TODO: make this work
+        return (
+            this._piecesBoard[8 - (position[1] - '0')][parseInt(position[0], 10) - 97]
+        );
     }
 }
 
