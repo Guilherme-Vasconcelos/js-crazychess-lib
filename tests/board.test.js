8174a8c64a57cb364ae5fb15dc79a94dc5a9f395
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2020-07-21 00:38:28 -0300

    All valid initial positions tests are done

--- tests/board.test.js
+++ tests/board.test.js
@@ -10,54 +10,46 @@ import { WHITE_PIECE_COLOR, BLACK_PIECE_COLOR } from '../src/constants.js';
 
 test('Valid initial positions', () => {
     let board = new Board();
-    expect(board._getPiece('a1').name).toBe('R');
-    expect(board._getPiece('b1').name).toBe('N');
-    expect(board._getPiece('c1').name).toBe('B');
-    expect(board._getPiece('d1').name).toBe('Q');
-    expect(board._getPiece('e1').name).toBe('K');
-    expect(board._getPiece('f1').name).toBe('B');
-    expect(board._getPiece('g1').name).toBe('N');
-    expect(board._getPiece('h1').name).toBe('R');
-    expect(board._getPiece('a2').name).toBe('P');
-    expect(board._getPiece('b2').name).toBe('P');
-    expect(board._getPiece('c2').name).toBe('P');
-    expect(board._getPiece('d2').name).toBe('P');
-    expect(board._getPiece('e2').name).toBe('P');
-    expect(board._getPiece('f2').name).toBe('P');
-    expect(board._getPiece('g2').name).toBe('P');
-    expect(board._getPiece('h2').name).toBe('P');
-    expect(board._getPiece('a8').name).toBe('r');
-    expect(board._getPiece('b8').name).toBe('n');
-    expect(board._getPiece('c8').name).toBe('b');
-    expect(board._getPiece('d8').name).toBe('q');
-    expect(board._getPiece('e8').name).toBe('k');
-    expect(board._getPiece('f8').name).toBe('b');
-    expect(board._getPiece('g8').name).toBe('n');
-    expect(board._getPiece('h8').name).toBe('r');
-    expect(board._getPiece('a7').name).toBe('p');
-    expect(board._getPiece('b7').name).toBe('p');
-    expect(board._getPiece('c7').name).toBe('p');
-    expect(board._getPiece('d7').name).toBe('p');
-    expect(board._getPiece('e7').name).toBe('p');
-    expect(board._getPiece('f7').name).toBe('p');
-    expect(board._getPiece('g7').name).toBe('p');
-    expect(board._getPiece('h7').name).toBe('p');
-    expect(board._getPiece('a5').name).toBe('.');
-    expect(board._getPiece('b4').name).toBe('.');
-    expect(board._getPiece('c3').name).toBe('.');
-    expect(board._getPiece('a1').color).toBe(WHITE_PIECE_COLOR);
-    expect(board._getPiece('b1').color).toBe(WHITE_PIECE_COLOR);
-    expect(board._getPiece('c1').color).toBe(WHITE_PIECE_COLOR);
-    expect(board._getPiece('d1').color).toBe(WHITE_PIECE_COLOR);
-    expect(board._getPiece('g1').color).toBe(WHITE_PIECE_COLOR);
-    expect(board._getPiece('h1').color).toBe(WHITE_PIECE_COLOR);
-    expect(board._getPiece('a2').color).toBe(WHITE_PIECE_COLOR);
-    expect(board._getPiece('h2').color).toBe(WHITE_PIECE_COLOR);
-    expect(board._getPiece('a7').color).toBe(BLACK_PIECE_COLOR);
-    expect(board._getPiece('e7').color).toBe(BLACK_PIECE_COLOR);
-    expect(board._getPiece('h7').color).toBe(BLACK_PIECE_COLOR);
-    expect(board._getPiece('a8').color).toBe(BLACK_PIECE_COLOR);
-    expect(board._getPiece('b8').color).toBe(BLACK_PIECE_COLOR);
-    expect(board._getPiece('d8').color).toBe(BLACK_PIECE_COLOR);
-    expect(board._getPiece('f8').color).toBe(BLACK_PIECE_COLOR);
+    const coordsPositionsWhitePieces = [
+        // White pieces
+        ['a1', 'R'], ['b1', 'N'], ['c1', 'B'], ['d1', 'Q'],
+        ['e1', 'K'], ['f1', 'B'], ['g1', 'N'], ['h1', 'R'],
+        ['a2', 'P'], ['b2', 'P'], ['c2', 'P'], ['d2', 'P'],
+        ['e2', 'P'], ['f2', 'P'], ['g2', 'P'], ['h2', 'P']
+    ]
+
+    const coordsPositionsBlackPieces = [
+        // Black pieces
+        ['a8', 'r'], ['b8', 'n'], ['c8', 'b'], ['d8', 'q'],
+        ['e8', 'k'], ['f8', 'b'], ['g8', 'n'], ['h8', 'r'],
+        ['a7', 'p'], ['b7', 'p'], ['c7', 'p'], ['d7', 'p'],
+        ['e7', 'p'], ['f7', 'p'], ['g7', 'p'], ['h7', 'p']
+    ]
+
+    const coordsPositionsNullPieces = [
+        // Null pieces
+        ['a3', '.'], ['b3', '.'], ['c3', '.'], ['d3', '.'],
+        ['e3', '.'], ['f3', '.'], ['g3', '.'], ['h3', '.'],
+        ['a4', '.'], ['b4', '.'], ['c4', '.'], ['d4', '.'],
+        ['e4', '.'], ['f4', '.'], ['g4', '.'], ['h4', '.'],
+        ['a5', '.'], ['b5', '.'], ['c5', '.'], ['d5', '.'],
+        ['e5', '.'], ['f5', '.'], ['g5', '.'], ['h5', '.'],
+        ['a6', '.'], ['b6', '.'], ['c6', '.'], ['d6', '.'],
+        ['e6', '.'], ['f6', '.'], ['g6', '.'], ['h6', '.']
+    ]
+
+    coordsPositionsWhitePieces.forEach(([coord, name]) => {
+        expect(board._getPiece(coord).name).toBe(name);
+        expect(board._getPiece(coord).color).toBe(WHITE_PIECE_COLOR);
+    });
+
+    coordsPositionsBlackPieces.forEach(([coord, name]) => {
+        expect(board._getPiece(coord).name).toBe(name);
+        expect(board._getPiece(coord).color).toBe(BLACK_PIECE_COLOR);
+    });
+
+    coordsPositionsNullPieces.forEach(([coord, name]) => {
+        expect(board._getPiece(coord).name).toBe(name);
+        expect(board._getPiece(coord).color).toBe(null);
+    });
 });
